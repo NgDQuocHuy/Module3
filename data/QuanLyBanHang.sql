2ebfd97e9eb30a9981b5f12f5122ba62cefae675
QuocHuy <ngdquochuy241@gmail.com>
2022-08-26 08:40:08 +0700

    Data quản lý bán hàng

--- data/QuanLyBanHang.sql
+++ data/QuanLyBanHang.sql
@@ -1,129 +1,29 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: quanlybanhang
--- ------------------------------------------------------
--- Server version	8.0.30
+SELECT * FROM quanlybanhang.customer;
+INSERT INTO `quanlybanhang`.`customer` (`cID`, `cName`, `cAge`) 
+VALUES (1, 'Minh Quan', 10),
+		(2, 'Ngoc Oanh', 20),
+        (3, 'Hong Ha', 50);
+SELECT * FROM customer;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `customer`
---
-
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customer` (
-  `cID` int NOT NULL AUTO_INCREMENT,
-  `cName` varchar(30) NOT NULL,
-  `cAge` int NOT NULL,
-  PRIMARY KEY (`cID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orderdetail`
---
-
-DROP TABLE IF EXISTS `orderdetail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orderdetail` (
-  `oID` int DEFAULT NULL,
-  `pID` int DEFAULT NULL,
-  `odQTY` int DEFAULT '0',
-  KEY `oID` (`oID`),
-  KEY `pID` (`pID`),
-  CONSTRAINT `orderdetail_ibfk_1` FOREIGN KEY (`oID`) REFERENCES `orderr` (`oID`),
-  CONSTRAINT `orderdetail_ibfk_2` FOREIGN KEY (`pID`) REFERENCES `product` (`pID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orderdetail`
---
-
-LOCK TABLES `orderdetail` WRITE;
-/*!40000 ALTER TABLE `orderdetail` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orderdetail` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orderr`
---
-
-DROP TABLE IF EXISTS `orderr`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orderr` (
-  `oID` int NOT NULL AUTO_INCREMENT,
-  `cID` int DEFAULT NULL,
-  `oDate` datetime DEFAULT NULL,
-  `oTotalPrice` decimal(10,0) DEFAULT '0',
-  PRIMARY KEY (`oID`),
-  KEY `cID` (`cID`),
-  CONSTRAINT `orderr_ibfk_1` FOREIGN KEY (`cID`) REFERENCES `customer` (`cID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orderr`
---
-
-LOCK TABLES `orderr` WRITE;
-/*!40000 ALTER TABLE `orderr` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orderr` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `product`
---
-
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `product` (
-  `pID` int NOT NULL AUTO_INCREMENT,
-  `pName` varchar(30) NOT NULL,
-  `pPrice` decimal(10,0) DEFAULT '0',
-  PRIMARY KEY (`pID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `product`
---
-
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-08-24 10:35:47
+SELECT * FROM quanlybanhang.orderr;
+INSERT INTO `quanlybanhang`.`orderr` (`cID`, `oDate`, `oTotalPrice`) 
+VALUES ( 1, '2006/3/21', null),
+		( 2, '2006/3/23', null),
+        ( 1, '2006/3/16', null);
+        
+INSERT INTO `quanlybanhang`.`product` (`pName`,`pPrice`)
+VALUES ('May Giat', 3),
+		('Tu Lanh', 5),
+        ('Dieu Hoa', 7),
+        ('Quat', 1),
+        ('Bep Dien', 2);
+        
+INSERT INTO `quanlybanhang`.`orderdetail` (`oID`, `pID`, `odQTY`)
+VALUES (1, 1, 3),
+		(1, 3, 7),
+        (1, 4, 2),
+        (2, 1, 1),
+        (3, 1, 8),
+        (2, 5, 4),
+        (2, 3, 3);
+SELECT * FROM orderdetail
