05514f71428c34ef5a023044b5bdc64fe5e8f2e0
QuocHuy <ngdquochuy241@gmail.com>
2022-08-26 10:42:16 +0700

    Store Procedure

--- data/StoreProcedure.sql
+++ data/StoreProcedure.sql
@@ -16,3 +16,47 @@ END //
 DELIMITER ;
 
 call findAllCustomers();
+
+DELIMITER //
+CREATE PROCEDURE getCusById(
+ IN cusNum INT(11)
+)
+BEGIN 
+	SELECT * FROM customers 
+    WHERE customerNumber = cusNum;
+END //
+
+DELIMITER ;
+
+call getCusById(175);
+
+DELIMITER //
+CREATE PROCEDURE GetCustomersCountByCity(
+ IN in_city VARCHAR(50),
+ OUT total INT
+)
+BEGIN
+	SELECT COUNT(customerNumber)
+    INTO total
+    FROM customers
+    WHERE city = in_city;
+END//
+DELIMITER ;
+
+CALL GetCustomersCountByCity('Lyon', @total);
+SELECT @total;
+
+DELIMITER //
+	CREATE PROCEDURE SetCounter(
+		INOUT counter INT,
+        IN inc INT
+	)
+    BEGIN 
+		SET counter = counter + inc;
+	END//
+DELIMITER ;
+SET @counter = 1;
+CALL SetCounter(@counter, 1);
+CALL SetCounter(@counter, 1);
+CALL SetCounter(@counter, 5);
+SELECT @counter;
